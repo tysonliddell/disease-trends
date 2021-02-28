5d1a60f88277daff84ac76ec4fbd01797d8fa34c
Tyson Liddell <tysonliddell@hotmail.com>
2021-02-28 00:16:03 +0000

    Create serverless function for aggregated counts
    The main PubMed search page at https://pubmed.ncbi.nlm.nih.gov/ has some
    backend logic that aggregates publication count by year. This allows us
    to extract all of the aggregated data in a single request, rather than a
    single API request for each year, which seems to be all that the
    eutilities API allows. It's not the prettiest solution, but it performs
    like a rocket.

---
+++ lambdas/disease-by-year/disease-by-year.js
@@ -0,0 +1,54 @@
+const fetch = require('node-fetch')
+const jsdom = require('jsdom');
+
+/* Parse a string of html text returned by a PubMed search query and extract the
+ * aggregated publication counts grouped by year from the table used to
+ * render the histogram on the search results page.
+ *
+ * This page was the only place I could find this data already aggregated. It
+ * stops us having make a separate API call for every year that we are
+ * interested in to get the publication count.
+ **/
+const getPubMedCounts = (htmlText) => {
+  const htmlDoc = new jsdom.JSDOM(htmlText).window.document
+  const table = htmlDoc.getElementById('timeline-table')
+
+  const countByYear = []
+  const rowsWithoutHeader = Array.from(table.rows).slice(1)
+  rowsWithoutHeader.forEach(row => {
+    const [year, count] = [
+      parseInt(row.children[0].innerHTML),
+      parseInt(row.children[1].innerHTML)
+    ]
+    countByYear.push({year, count})
+  })
+  return countByYear
+}
+
+const handler = async function (event, context) {
+  try {
+    const term = event.queryStringParameters.term;
+    const url = `https://pubmed.ncbi.nlm.nih.gov/?term=${term}`;
+    const response = await fetch(url, {
+      headers: { Accept: 'text/html' },
+    })
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText }
+    }
+    const data = await response.text()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ counts: getPubMedCounts(data) }),
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: error.message }),
+    }
+  }
+}
+
+module.exports = { handler }
